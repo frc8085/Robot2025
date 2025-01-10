903fd6600f3668dc5f829b58a3aefdcce2a67895
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2025-01-10 15:50:32 -0500

    added section for controls and forward control

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,8 +1,9 @@
+
 package frc.robot;
 
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.Constants.ModuleConstants;
 
