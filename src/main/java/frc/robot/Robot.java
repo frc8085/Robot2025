79b155df4eb0bef801addaa1efe8e478176c1dee
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2025-01-10 16:32:09 -0500

    I lied :(

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,89 +1,69 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import frc.robot.Constants.DriveConstants;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-  private SparkMax motor;
-  private SparkMaxConfig motorConfig;
-  private SparkClosedLoopController closedLoopController;
-  private RelativeEncoder encoder;
+  SparkMax leftLeader;
+  SparkMax leftFollower;
+  SparkMax rightLeader;
+  SparkMax rightFollower;
+  XboxController joystick;
 
   public Robot() {
+    // Initialize the SPARKs
+    leftLeader = new SparkMax(DriveConstants.kFrontLeftDrivingCanId, MotorType.kBrushless);
+    leftFollower = new SparkMax(DriveConstants.kRearLeftDrivingCanId, MotorType.kBrushless);
+    rightLeader = new SparkMax(DriveConstants.kFrontRightDrivingCanId, MotorType.kBrushless);
+    rightFollower = new SparkMax(DriveConstants.kRearRightDrivingCanId, MotorType.kBrushless);
+
     /*
-     * Initialize the SPARK MAX and get its encoder and closed loop controller
-     * objects for later use.
+     * Create new SPARK MAX configuration objects. These will store the
+     * configuration parameters for the SPARK MAXes that we will set below.
      */
-    motor = new SparkMax(1, MotorType.kBrushless);
-    closedLoopController = motor.getClosedLoopController();
-    encoder = motor.getEncoder();
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
 
     /*
-     * Create a new SPARK MAX configuration object. This will store the
-     * configuration parameters for the SPARK MAX that we will set below.
+     * Set parameters that will apply to all SPARKs. We will also use this as
+     * the left leader config.
      */
-    motorConfig = new SparkMaxConfig();
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
+
+    // Apply the global config and invert since it is on the opposite side
+    rightLeaderConfig
+        .apply(globalConfig)
+        .inverted(true);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    leftFollowerConfig
+        .apply(globalConfig)
+        .follow(leftLeader);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    rightFollowerConfig
+        .apply(globalConfig)
+        .follow(rightLeader);
 
     /*
-     * Configure the encoder. For this specific example, we are using the
-     * integrated encoder of the NEO, and we don't need to configure it. If
-     * needed, we can adjust values like the position or velocity conversion
-     * factors.
-     */
-    motorConfig.encoder
-        .positionConversionFactor(1)
-        .velocityConversionFactor(1);
-
-    /*
-     * Configure the closed loop controller. We want to make sure we set the
-     * feedback sensor as the primary encoder.
-     */
-    motorConfig.closedLoop
-        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-        // Set PID values for position control. We don't need to pass a closed
-        // loop slot, as it will default to slot 0.
-        .p(0.4)
-        .i(0)
-        .d(0)
-        .outputRange(-1, 1)
-        // Set PID values for velocity control in slot 1
-        .p(0.0001, ClosedLoopSlot.kSlot1)
-        .i(0, ClosedLoopSlot.kSlot1)
-        .d(0, ClosedLoopSlot.kSlot1)
-        .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
-        .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
-
-    motorConfig.closedLoop.maxMotion
-        // Set MAXMotion parameters for position control. We don't need to pass
-        // a closed loop slot, as it will default to slot 0.
-        .maxVelocity(1000)
-        .maxAcceleration(1000)
-        .allowedClosedLoopError(1)
-        // Set MAXMotion parameters for velocity control in slot 1
-        .maxAcceleration(500, ClosedLoopSlot.kSlot1)
-        .maxVelocity(6000, ClosedLoopSlot.kSlot1)
-        .allowedClosedLoopError(1, ClosedLoopSlot.kSlot1);
-
-    /*
-     * Apply the configuration to the SPARK MAX.
+     * Apply the configuration to the SPARKs.
      *
      * kResetSafeParameters is used to get the SPARK MAX to a known state. This
      * is useful in case the SPARK MAX is replaced.
@@ -92,48 +72,72 @@ public class Robot extends TimedRobot {
      * the SPARK MAX loses power. This is useful for power cycles that may occur
      * mid-operation.
      */
-    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    // Initialize dashboard values
-    SmartDashboard.setDefaultNumber("Target Position", 0);
-    SmartDashboard.setDefaultNumber("Target Velocity", 0);
-    SmartDashboard.setDefaultBoolean("Control Mode", false);
-    SmartDashboard.setDefaultBoolean("Reset Encoder", false);
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    if (SmartDashboard.getBoolean("Control Mode", false)) {
-      /*
-       * Get the target velocity from SmartDashboard and set it as the setpoint
-       * for the closed loop controller with MAXMotionVelocityControl as the
-       * control type.
-       */
-      double targetVelocity = SmartDashboard.getNumber("Target Velocity", 0);
-      closedLoopController.setReference(targetVelocity, ControlType.kMAXMotionVelocityControl,
-          ClosedLoopSlot.kSlot1);
-    } else {
-      /*
-       * Get the target position from SmartDashboard and set it as the setpoint
-       * for the closed loop controller with MAXMotionPositionControl as the
-       * control type.
-       */
-      double targetPosition = SmartDashboard.getNumber("Target Position", 0);
-      closedLoopController.setReference(targetPosition, ControlType.kMAXMotionPositionControl,
-          ClosedLoopSlot.kSlot0);
-    }
+    // Initialize joystick
+    joystick = new XboxController(0);
   }
 
   @Override
   public void robotPeriodic() {
-    // Display encoder position and velocity
-    SmartDashboard.putNumber("Actual Position", encoder.getPosition());
-    SmartDashboard.putNumber("Actual Velocity", encoder.getVelocity());
+    // Display the applied output of the left and right side onto the dashboard
+    SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
+    SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
+  }
 
-    if (SmartDashboard.getBoolean("Reset Encoder", false)) {
-      SmartDashboard.putBoolean("Reset Encoder", false);
-      // Reset the encoder position to 0
-      encoder.setPosition(0);
-    }
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    /**
+     * Get forward and rotation values from the joystick. Invert the joystick's
+     * Y value because its forward direction is negative.
+     */
+    double forward = -joystick.getLeftY();
+    double rotation = joystick.getRightX();
+
+    /*
+     * Apply values to left and right side. We will only need to set the leaders
+     * since the other motors are in follower mode.
+     */
+    leftLeader.set(forward + rotation);
+    rightLeader.set(forward - rotation);
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
   }
 }
